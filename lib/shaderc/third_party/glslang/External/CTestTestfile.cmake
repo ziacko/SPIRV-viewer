4be7f06a760465ebe8fd8a9b7959564024d40da2
ziyad barakat <ziyadbarakat@gmail.com>
2016-07-31 03:12:48 +1000

    removed useless  files

--- lib/shaderc/third_party/glslang/External/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ziyad/Documents/SPIRV-viewer/lib/shaderc/third_party/glslang/External
-# Build directory: C:/Users/ziyad/Documents/SPIRV-viewer/lib/shaderc/third_party/glslang/External
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
